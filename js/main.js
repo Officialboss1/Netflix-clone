222474e2f30f697af9bc908c6aea116b1cf6bbc7
officialboss <mikeedaboss1@iclou.com>
2022-08-27 14:28:41 +0100

    just added the js file

---
+++ js/main.js
@@ -0,0 +1,29 @@
+const tabItems = document.querySelectorAll('.tab-item');
+const tabContentItems = document.querySelectorAll('.tab-content-item');
+
+// select tab content item
+function selectItem(e){
+    removeBorder();
+    removeShow();
+    // Add border to current tab
+    this.classList.add('tab-border');
+    //Grab content item from Dom
+    const tabContentItem = document.querySelector(`#${this.id}-content`);
+    // tab content item 
+    tabContentItem.classList.add('Show');
+}
+
+function removeBorder(){
+    tabItems.forEach(item => {
+        item.classList.remove('tab-border')
+    });
+}
+
+function removeShow(){
+    tabContentItems.forEach(item =>{
+        item.classList.remove('Show')
+    });
+}
+// Listen for tab clicks
+tabItems.forEach(item => 
+    {item.addEventListener('click',selectItem)});
